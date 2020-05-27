4418d86637d85fbdb53db4e8149610a6ea9eca72
Georg ´Brantegger <georg.brantegger@gmail.com>
2020-05-27 11:49:00 +0200

    figured out how convex hull works

--- first_steps.py
+++ first_steps.py
@@ -1,24 +1,22 @@
 import numpy as np 
 import scipy
+import scipy.spatial as spat
+from scipy.spatial import ConvexHull
 from matplotlib import pyplot as plt
 from polygon_generator import polygon2D
-
 np.random.seed(5)
 
-polygon_vertices = polygon2D(8,5)
-#print(f"Array of vertices: \n {polygon_vertices}")
-#print(f"First vertex: \n {polygon_vertices[:,0]}")
-#polygon_obj = scipy.spatial.ConvexHull(polygon_vertices)
-
+points = polygon2D(100,50)
+#print(np.shape(points))
+#print(f"Array of vertices: \n {points}")
+#print(f"First vertex: \n {points[:,0]}")
+polygon = ConvexHull(np.transpose(points))
 
 fig1 = plt.figure()
 ax11 = fig1.add_axes()
-plt.scatter(polygon_vertices[0,:],polygon_vertices[1,:])
+plt.scatter(points[0,:],points[1,:])
+for simplex in polygon.simplices:
+    plt.plot(points[0,simplex], points[1,simplex], 'k-')
 plt.show()
-
-
-
-
-
 
 
--- polygon_generator.py
+++ polygon_generator.py
@@ -4,6 +4,6 @@ import scipy
 def polygon2D(n,r):
     # n = number of vertices
     # r = range = max distance from (0,0,0)
-    vertices = r*np.round(np.random.rand(2,n),2)
+    vertices = np.round(r*np.random.rand(2,n),2)
     return vertices
 
